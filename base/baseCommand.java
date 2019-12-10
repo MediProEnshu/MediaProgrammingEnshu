c12a5b66dcc130550324de623c0156915806ad5d
coldTOUFU <ironasam.5989@ezweb.ne.jp>
2019-12-10 20:52:14 +0900

    BaseSelect完成

--- base/baseCommand.java
+++ base/baseCommand.java
@@ -4,56 +4,65 @@ import java.awt.event.*;
 import java.util.*;
 
 /* コマンドオブジェクト */
+// コマンドを保持するためのオブジェクトなので、外部的にはパラメタをもつJLabelとして振舞う.
+// そのため、 View, Controllerは不要 */
 
 /* Model: コマンド文を保持 */
 /////////////////////////////////////////////////////////
 class BaseCommandModel {
-  private String command;
+    private int serialID; //コマンド列のコマンド1つ1つが持つ. 0から始める.
+    private String command;
 
-  public BaseCommandModel(String text) {
-    command = text;
-  }
+    public BaseCommandModel(int num, String text) {
+        this.serialID = num;
+        this.command = text;
+    }
 
-  public String get(){
-    return this.command;
-  }
+    public String getCommand() {
+        return this.command;
+    }
+
+    public int getSerialID() {
+        return this.serialID;
+    }
 }
 
 
-/* View: コマンドラベルを表示 */
+/* Object: コマンドラベルオブジェクト(Modelから取り出した情報を使う) */
+/* Modelを与えられると対応する JLabel+パラメタ を作るオブジェクト. */
 /////////////////////////////////////////////////////////
-class BaseCommandView extends JLabel {
-  public BaseCommandView(BaseCommandModel model) {
-    this.setText(model.get());
-  }
+class BaseCommandLabel extends JLabel {
+    public BaseCommandLabel(BaseCommandModel model) {
+        this.setText(model.getCommand());
+    }
 }
 
 
 /* main: テスト用 */
 /////////////////////////////////////////////////////////
-class BaseCommand extends JFrame {
-  public BaseCommand() {
-    this.setTitle("BaseSelect テスト");
-    this.setLayout(new GridLayout(6, 1));
-    /* コマンド登録 */
-    BaseCommandModel command1 = new BaseCommandModel("チーカマ");
-    BaseCommandModel command2 = new BaseCommandModel("味噌田楽");
-    BaseCommandModel command3 = new BaseCommandModel("しぐれ煮");
-    BaseCommandModel command4 = new BaseCommandModel("煮っころがし");
-    BaseCommandModel command5 = new BaseCommandModel("もつ煮");
-    BaseCommandModel command6 = new BaseCommandModel("生ハム");
+class BaseCommandTest extends JFrame {
+    public BaseCommandTest() {
+        this.setTitle("BaseSelect テスト");
+        this.setLayout(new GridLayout(6, 1));
+        /* コマンド登録 */
+        BaseCommandModel command1 = new BaseCommandModel(0, "チーカマ");
+        BaseCommandModel command2 = new BaseCommandModel(1, "味噌田楽");
+        BaseCommandModel command3 = new BaseCommandModel(2, "しぐれ煮");
+        BaseCommandModel command4 = new BaseCommandModel(3, "煮っころがし");
+        BaseCommandModel command5 = new BaseCommandModel(4, "もつ煮");
+        BaseCommandModel command6 = new BaseCommandModel(5, "生ハム");
 
-    this.add(new BaseCommandView(command1)); this.add(new BaseCommandView(command2));
-    this.add(new BaseCommandView(command3)); this.add(new BaseCommandView(command4));
-    this.add(new BaseCommandView(command5)); this.add(new BaseCommandView(command6));
+        this.add(new BaseCommandLabel(command1)); this.add(new BaseCommandLabel(command2));
+        this.add(new BaseCommandLabel(command3)); this.add(new BaseCommandLabel(command4));
+        this.add(new BaseCommandLabel(command5)); this.add(new BaseCommandLabel(command6));
 
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.pack();
-    this.setSize(300,200);
-    this.setVisible(true);
-  }
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setSize(300,200);
+        this.setVisible(true);
+    }
 
-  public static void main(String argv[]) {
-    new BaseCommand();
-  }
+    public static void main(String argv[]) {
+        new BaseCommandTest();
+    }
 }
