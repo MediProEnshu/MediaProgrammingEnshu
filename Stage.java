674d486288a6e71cb2281d6390c64e4355405004
ONZIUEC <51269199+ONZIUEC@users.noreply.github.com>
2019-12-22 20:47:12 +0900

    新しい方式(未完成)

--- Stage.java
+++ Stage.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Graphics2D;
 import javax.imageio.ImageIO;
 import javax.imageio.plugins.tiff.TIFFDirectory;
 import java.awt.image.BufferedImage;
@@ -10,51 +9,73 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.nio.file.Paths;
-import java.awt.Point;
-class CreateMap {//マップを生成するクラス
-    private char[][] data;
-    private int hor;
-    private int ver;
-    
-    public CreateMap() {
-        data = readMapData();
-        hor = data[0].length;
-        ver = data.length;
+import java.util.Iterator;
+
+class Map {//マップを生成するクラス
+    private char[][] stageMapData;
+    private char [][] charaMapData;
+    private BaseCharacter [][] charaMap;
+    private int horizontalLength;
+    private int verticalLength;
+    private ArrayList character1 = new ArrayList();
+    private ArrayList character2 = new ArrayList();
+    public Map(String stageMapFile) {
+        stageMapData = readstageMapData(stageMapFile);
+        horizontalLength = stageMapData[0].length;
+        verticalLength = stageMapData.length;
+        charaMapData = new char [verticalLength][horizontalLength];
+        BaseCharacter c = new BaseCharacter(5, "chara1", 32, 32, 1, '1');
+        character1.add(c);
     }
-    private char[][] readMapData() {//map.txtから文字列を読み込む
-        ArrayList<String> line = new ArrayList<>();//読み込んだ文字列を格納する
-        Scanner scanner = new Scanner(getClass().getResourceAsStream("map1.txt"));//map.txtを読み込む
+    private char[][] readstageMapData(String stageMapFile) {//map.txtから文字列を読み込む
+        ArrayList<String> arrayHorizontalLine = new ArrayList<>();//読み込んだ文字列を格納する
+        Scanner scanner = new Scanner(getClass().getResourceAsStream(stageMapFile));//map.txtを読み込む
             while (scanner.hasNextLine()) {//空行になるまで行を読み込む
                 String mapstring = scanner.nextLine();//mapstringに読み込んだ行を格納
-                line.add(mapstring.replace("\t", ""));//改行文字を消す
+                arrayHorizontalLine.add(mapstring.replace("\t", ""));//改行文字を消す
             }
-        int lineCount = line.size();//読み込んだ行数を出す
-        char[][] maparray = new char[lineCount][];//読み込んだ行数のサイズの行を持つ二次元配列を作る
+        int lineCount = arrayHorizontalLine.size();//読み込んだ行数を出す
+        char[][] stageMapArray = new char[lineCount][];//読み込んだ行数のサイズの行を持つ二次元配列を作る
         for (int i = 0; i < lineCount; i++) {//char型配列に読み込んだ文字列を変換
-            maparray[i] = line.get(i).toCharArray();
+            stageMapArray[i] = arrayHorizontalLine.get(i).toCharArray();
         }
-        return maparray;
+        return stageMapArray;
     }
-    public char getCode(int x, int y) {//ある位置のマップの記号返す
+    public void charaMapInit() {
+        for(int i = 0; i < horizontalLength; i++) {
+            for(int j = 0; j < verticalLength; j++) {
+                charaMapData[j][i] = '.';
+            }
+            for(Iterator it = character1.iterator(); it.hasNext(); ){
+                BaseCharacter c = (BaseCharacter)(it.next());
+                charaMapData[c.getPosition().y][c.getPosition().x] = c.getClassType();
+             }
+        }
+    }
+    public char getStageMapCode(int x, int y) {//ある位置のマップの記号返す
         // 引数チェックは省略
-        return data[y][x];
+        return stageMapData[y][x];
+    }
+    public char getCharaMapCode(int x, int y) {//ある位置のマップの記号返す
+        // 引数チェックは省略
+        return charaMapData[y][x];
     }
     public void setCode(int x, int y, char tile) {
-        data[y][x] = tile;
+        charaMapData[y][x] = tile;
     }
-    public int gethor() {
-        return hor;
+    public int getHorizontalLength() {
+        return horizontalLength;
     }
-    public int getver() {
-        return ver;
+    public int getVerticalLength() {
+        return verticalLength;
     }
 }
 class ImportTile extends Component {//タイルチップを読み込むクラス
     public final int TileSize = 32;//タイルのサイズ
     static String codemap = ".0123456789ABCDE";//タイルと文字の対応のための文字列
     private BufferedImage tileset = null;//タイルセットを読み根で格納する
-    public ImportTile()throws IOException {
-        String imageFilename = "TileSet.PNG";
+    public ImportTile(String file)throws IOException {
+        String imageFilename = file;
         this.tileset = ImageIO.read(new File(imageFilename));
     }
     public Dimension getPreferredSize() {
@@ -73,83 +94,6 @@ class ImportTile extends Component {//タイルチップを読み込むクラス
         return tileset.getSubimage(x, y, TileSize, TileSize);
     }
 }
-class BaseCharacterModel {
-    private int x = 0;
-    private int y = 0;
-
-    private int maxHitPoint;
-    private int hitPoint;
-    private String imagePath;
-    boolean selected = false;
-    public BaseCharacterModel(int hp, String pathString) {
-        if(hp < 0){ hp = 0; } // 不適切なヒットポイントの修正
-        maxHitPoint = hp;
-        hitPoint = hp;
-        imagePath = pathString;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String pathString) {
-        imagePath = pathString;
-    }
-    public void setPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    public Point getPosition() {
-        Point p = new Point();
-        p.x = x;
-        p.y = y;
-        return p;
-    }
-
-    /* ヒットポイントを返す. */
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    /* このキャラクターは絶命した? */
-    public boolean isDead() {
-        return hitPoint <= 0;
-    }
-
-    /* damageが0以上なら損傷、0未満なら治療. */
-    public void giveDamage(int damage) {
-        hitPoint -= damage;
-        if(hitPoint < 0)           { hitPoint = 0; }
-        if(hitPoint > maxHitPoint) { hitPoint = maxHitPoint; }
-    }
-}
-/* Object: キャラクターの表示 */
-/////////////////////////////////////////////////////////
-class BaseCharacterLabel extends JLabel {
-    private BaseCharacterModel model;
-    BufferedImage icon;
-    public BaseCharacterLabel(BaseCharacterModel bcm) throws IOException{
-        model = bcm;
-        this.setCharacterImage();
-    }
-    /* キャラクターの画像をセット */
-    public void setCharacterImage() throws IOException{
-        icon = ImageIO.read(new File(model.getImagePath()));
-    }
-}
-
-class DrawCharacter extends JPanel {
-    BaseCharacterModel model;
-    BaseCharacterLabel label;
-    public DrawCharacter() {
-        model = new BaseCharacterModel(5, "./Chara.png");
-        try {
-            label = new BaseCharacterLabel(model);
-        } catch (IOException e) {
-            
-        }
-    }
-}
 class GameScreen extends JPanel implements MouseListener{
     static final int startX = 0;
     static final int startY = 0;
@@ -163,47 +107,44 @@ class GameScreen extends JPanel implements MouseListener{
     DrawCharacter chara;
     boolean rect_flag = false;
     BufferedImage mapImage;
-    CreateMap map = new CreateMap();
+    BufferedImage charaImage;
+    CreateMap map = new CreateMap("map1.txt");
     public GameScreen() throws IOException {
-        this.mapImage = createImage();
+        this.mapImage = createImage("Tileset.PNG", true);
+        //this.charaImage = createImage("Charaset.PNG");
         this.width = mapImage.getWidth();//サイズ設定
         this.height = mapImage.getHeight();
         addMouseListener(this);
         initialize();
-        MakeChara();
     }
     void initialize() {//ゲーム画面の初期位置の設定
         this.x = TileSize * (startX);
         this.y = TileSize * (startY);
     }
-    void MakeChara() throws IOException{
-        chara = new DrawCharacter();
-        chara.model.setPosition(64, 64);
-    }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        try {
-            mapImage = createImage();
-        } catch (IOException e) {
-
-        }
         g.drawImage(mapImage.getSubimage(x, y, getWidth(), getHeight()), 0, 0, this);
-        g.drawImage(chara.label.icon, chara.model.getPosition().x, chara.model.getPosition().y, this);
+        g.drawImage(charaImage.getSubimage(x, y, getWidth(), getHeight()), 0, 0, this);
         g.setColor(Color.RED);
-        if(rect_flag == true)
+        if(rect_flag == true) {
         g.drawRect(rect_x, rect_y, TileSize, TileSize);
+        }
     }
-    public BufferedImage createImage() throws IOException{//マップを
-        BufferedImage bi = new BufferedImage(TileSize * 124, TileSize * 120, BufferedImage.TYPE_INT_RGB);
+    public BufferedImage createImage(String fileName, boolean type) throws IOException{//マップを
+        BufferedImage bi = new BufferedImage(TileSize * 124, TileSize * 120, BufferedImage.TYPE_INT_ARGB);
         Graphics g = bi.getGraphics();
-        ImportTile tileset = new ImportTile();//タイルセットを呼び出す
-        int verticalLength = map.getver();//縦の長さ
-        int horizontalLength = map.gethor();//横の長さ
+        ImportTile tileset = new ImportTile(fileName);//タイルセットを呼び出す
+        int verticalLength = map.getVerticalLength();//縦の長さ
+        int horizontalLength = map.getHorizontalLength();//横の長さ
         int gY = 0;//タイルをどこから書き始めるか。この設定だと大体左上
         for (int y = 0; y < verticalLength; y++) {
             int gX = 0;
             for (int x = 0; x < horizontalLength; x++) {
-                g.drawImage(tileset.getTile(map.getCode(x, y)), gX, gY, null);//描画
+                if(type) {
+                g.drawImage(tileset.getTile(map.getStageMapCode(x, y)), gX, gY, null);//描画
+                } else {
+                    g.drawImage(tileset.getTile(map.getStageMapCode(x, y)), gX, gY, null);//描画
+                }
                 gX += TileSize;//タイルの大きさ分横にずらす
             }
             gY += TileSize;//タイルの大きさ分縦にずらす
@@ -237,15 +178,98 @@ class GameScreen extends JPanel implements MouseListener{
     public void mouseExited(MouseEvent e)  { }
     public void mousePressed(MouseEvent e) { }
 }
+class BaseCharacter {
+    private int x = 0;
+    private int y = 0;
+    private int attackPoint;
+    private int defencePoint;
+    private int maxHitPoint;
+    private int hitPoint;
+    private String imagePath;
+    private boolean selected = false;
+    private String name; 
+    private int player;
+    BufferedImage graphic;
+    private char classType;
+    public BaseCharacter(int hp, String name, int x, int y, int player, char classType) {
+        if(hp < 0){ hp = 0; } // 不適切なヒットポイントの修正
+        maxHitPoint = hp;
+        hitPoint = hp;
+        setGraphic(imagePath);
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.player = player;
+        this.classType = classType;
+    }
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String pathString) {
+        imagePath = pathString;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPlayer() {
+        return player;
+    }
+    public void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public boolean getSelected() {
+        return selected;
+    }
+    public Point getPosition() {
+        Point p = new Point();
+        p.x = x;
+        p.y = y;
+        return p;
+    }
+    public char getClassType() {
+        return classType;
+    }
+    /* ヒットポイントを返す. */
+    public int getHitPoint() {
+        return hitPoint;
+    }
+    public int getAttackPoint() {
+        return attackPoint;
+    }
+    public int getDefencePoint() {
+        return defencePoint;
+    }
+    /* このキャラクターは絶命した? */
+    public boolean isDead() {
+        return hitPoint <= 0;
+    }
+    void attack(BaseCharacter c) {
+//
+    }
+    /* damageが0以上なら損傷、0未満なら治療. */
+    public void giveDamage(int attackPoint) {
+        int damage = attackPoint - getAttackPoint(); 
+        hitPoint -= damage;
+        if(hitPoint < 0)           { hitPoint = 0; }
+        if(hitPoint > maxHitPoint) { hitPoint = maxHitPoint; }
+    }
+    public void setGraphic(String filepath) throws IOException{   
+        this.graphic =  ImageIO.read(new File(filepath));
+    }
+}
 class Frame extends JFrame {
     public Frame() throws IOException {
         JPanel panel = new JPanel();
         this.setTitle("BaseCharacter テスト");
         GameScreen screen = new GameScreen();
         panel.setLayout(new GridLayout(1, 1));
+        panel.setBackground(Color.WHITE);
         panel.setPreferredSize(new Dimension(screen.width, screen.height));
         panel.add(screen);
         this.add(panel, BorderLayout.CENTER);
+        this.setBackground(Color.RED);
         this.pack(); 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
